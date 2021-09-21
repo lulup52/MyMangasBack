0140fe2cc8dee899c045b25c13d9f2e0e34717ed
lulup52 <lucas-p52@hotmail.fr>
2021-09-21 16:40:20 +0200

    création route delete collection

--- routes/collection.js
+++ routes/collection.js
@@ -19,7 +19,7 @@ router.get("/",(req, res) =>{
 /* ----- GET all tomes in collections by user id  ----- */
 
 router.get("/tome_collection/:userid",(req, res) =>{
-    let userId=req.params.userid
+    let userId = req.params.userid
     connection.query('SELECT * from collection where user_id=?',[userId], (err, results) => {
       if (err) {
         res.status(500).send('Erreur lors de la récupération des collections');
@@ -31,7 +31,7 @@ router.get("/tome_collection/:userid",(req, res) =>{
 /* ----- GET all series in collection by user id ----- */
 
 router.get("/serie_collection/:userid", (req, res) => {
-    userId =  req.params.userid
+  let userId =  req.params.userid
     connection.query(
       " select user_id,user_name , serie.id AS serieId,serie.serie_title, nbr_of_tome, serie.ilustration, author from collection JOIN tome ON collection.tome_id=tome.id JOIN serie on serie_id=serie.id JOIN user ON collection.user_id=user.id WHERE user_id=? GROUP BY serie_title;",
       [userId],
@@ -51,10 +51,10 @@ router.get("/serie_collection/:userid", (req, res) => {
 /* ----- GET all tomes in a serie by collection and user id ----- */
 
 router.get("/alltomes_collection/:userid/:serieid", (req, res) => {
-  userid =  req.params.userid
-  serieid =  req.params.serieid
+  let userid =  req.params.userid
+  let serieid =  req.params.serieid
     connection.query(
-      "select user_id ,user_name,tome.id AS tomeId, serie.id AS seriId, serie.serie_title, subtitle, num_tome, tome.ilustration, tome_sumary from collection join tome ON collection.tome_id=tome.id JOIN serie ON tome.serie_id=serie.id JOIN user ON collection.user_id=user.id where user_id=? AND tome.serie_id=?;",
+      "select user_id ,user_name,tome.id AS tomeId, serie.id AS seriId, serie.serie_title, subtitle, num_tome, tome.ilustration, tome_sumary from collection join tome ON collection.tome_id=tome.id JOIN serie ON tome.serie_id=serie.id JOIN user ON collection.user_id=user.id where user_id=? AND tome.serie_id=? ORDER BY num_tome;",
       [userid, serieid],
     
       (err, results) => {
@@ -74,7 +74,7 @@ router.get("/alltomes_collection/:userid/:serieid", (req, res) => {
 /* ----- Get all series not include in user colection----- */
 
 router.get("/serie_notin_collection/:userid", (req, res) => {
-  userId =  req.params.userid
+  let userId =  req.params.userid
   connection.query(
     " select user_id,user_name , serie.id AS serieId,serie.serie_title, nbr_of_tome, serie.ilustration, author from collection JOIN tome ON collection.tome_id=tome.id JOIN serie on serie_id=serie.id JOIN user ON collection.user_id=user.id WHERE user_id=? GROUP BY serie_title;",
     [userId],
@@ -102,8 +102,8 @@ router.get("/serie_notin_collection/:userid", (req, res) => {
 /* ----- Get all tomes not include in user colection by series ----- */
 
 router.get("/tome_notin_collection_by_serie/:userid/:serieid", (req, res) => {
-  userId =  req.params.userid
-  serieId =  req.params.serieid
+  let userId =  req.params.userid
+  let serieId =  req.params.serieid
   connection.query(
     //---we get all tomes in collection by serie
     "SELECT user_name, user.id as user_id, tome.serie_title, serie_id, subtitle,num_tome, tome.id FROM tome JOIN collection ON tome.id = collection.tome_id join user ON collection.user_id=user.id WHERE user.id = ? AND serie_id=?",
@@ -119,7 +119,7 @@ router.get("/tome_notin_collection_by_serie/:userid/:serieid", (req, res) => {
               console.log(tomesToCheck)
                 let idTomeToRemove = tomesToCheck.map(e => e.id)
                 connection.query(    
-                  "select tome.id as tome_id, serie.id as serie_id, serie.serie_title, tome.ilustration, subtitle, num_tome, tome_sumary from tome join serie on serie.id=tome.serie_id where serie.id = ? and tome.id NOT IN (?);",
+                  "select tome.id as tomeId, serie.id as serie_id, serie.serie_title, tome.ilustration, subtitle, num_tome, tome_sumary from tome join serie on serie.id=tome.serie_id where serie.id = ? and tome.id NOT IN (?);",
                   [serieId, idTomeToRemove],
                   (error, result ) => {
                     if(error) {
@@ -146,8 +146,8 @@ router.get("/tome_notin_collection_by_serie/:userid/:serieid", (req, res) => {
 /* ----- post a tome(id) in user's(id) colection----- */
 
 router.post("/tome_collection_add/:userid/:tomeid", (req, res) => {
-  userId =  req.params.userid
-  tomeid =  req.params.tomeid
+  let userId =  req.params.userid
+  let tomeid =  req.params.tomeid
 
     connection.query(
       "INSERT INTO collection (tome_id, user_id) VALUES (?,?);",
@@ -166,5 +166,33 @@ router.post("/tome_collection_add/:userid/:tomeid", (req, res) => {
       });
   });
 
+/*---------------------remove a tome from a collection-------------------------*/
 
+router.delete("/delete/:userid/:tomeid",(req, res) =>{
+  let userId =  req.params.userid
+  let tomeid =  req.params.tomeid
+  connection.query('SELECT * FROM collection WHERE user_id = ? AND tome_id=?', [userId ,tomeid], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des collections');
+    } else {
+      if(results.length === 0) {
+        res.status(404).json("ce tome n'est pas présent dans cete collection");
+
+      } else {
+        
+        connection.query(    
+          "DELETE FROM collection WHERE user_id = ? AND tome_id=?",
+          [userId, tomeid],
+          (error, result ) => {
+            if (err) {
+              res.status(500).send('Erreur lors de la récupération des collections');
+            } else {
+              
+              res.status(200).json("tome suprimé avec succès");
+            }
+        })
+      }
+    }
+  });
+});
 module.exports = router;
--- routes/series.js
+++ routes/series.js
@@ -22,7 +22,7 @@ router.get("/",(req, res) =>{
 router.get("/alltomes_serie/:serieid", (req, res) => {
   serieId =  req.params.serieid
     connection.query(
-      "select tome.id AS tomeID, num_tome,serie_id, serie.serie_title, subtitle, tome_sumary FROM serie JOIN tome ON tome.serie_id=serie.id WHERE serie.id =?;",
+      "select tome.id AS tomeId, num_tome,serie_id, serie.serie_title, subtitle, tome_sumary FROM serie JOIN tome ON tome.serie_id=serie.id WHERE serie.id =?;",
       [serieId],
     
       (err, results) => {
