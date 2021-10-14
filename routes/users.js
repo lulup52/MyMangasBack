dd1e317fcbbda761389f7b621b88ea93d9aff48e
lulup52 <lucas-p52@hotmail.fr>
2021-10-14 10:08:22 +0200

    gestion de la vérification user + mdp sans jwt

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,27 @@ router.get("/",(req, res) =>{
       }
     });
 });
+/* ----- GET user if mdp and username match ----- */
+
+router.post("/login",(req, res) =>{
+  console.log(req.body)
+    userName = req.body.username
+    passWord = req.body.mdp
+    connection.query('SELECT * from user WHERE user_name=? AND pass=?',[userName,passWord], (err, results) => {
+      if (err) {
+        res.status(500).send('Erreur lors de la récupération des lectures');
+      } else {
+        if(results.length === 0) {
+          res.status(200).json(`données saisies éronnées`);
+
+        } else {
+
+          res.status(200).json(`${userName} is loged in with the id ${results[0].id}`);
+        }
+
+      }
+    });
+});
 
 
 module.exports = router;
